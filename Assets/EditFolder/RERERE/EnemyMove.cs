35894cc24148419ada1ad17a43322e59454a5d7d
RERERE-0527 <167706848+RERERE-0527@users.noreply.github.com>
2024-05-26 15:35:23 +0900

    REREREシーンにPlayrrを追加

--- Assets/EditFolder/RERERE/EnemyMove.cs
+++ Assets/EditFolder/RERERE/EnemyMove.cs
@@ -8,10 +8,13 @@ public class EnemyMove : MonoBehaviour
     [SerializeField] GameObject PL;
     Vector2 PLpos;
     Vector2 pos;
+    int r;
+    Rigidbody rb;
+    [SerializeField]float Movespeed=5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,6 +22,16 @@ public class EnemyMove : MonoBehaviour
     {
         PLpos = PL.transform.position;
         pos=transform.position;
-
+        //enemy‚©‚çŒ©‚ÄPL‚ª‚Ç‚¿‚ç‚É‚¢‚é‚©”»’è‚µ‚»‚Ì•ûŒü‚Éi‚Ş
+        if(PLpos.x-pos.x<0)
+        {
+            r = 1;
+        }
+        else
+        {
+            r = -1;
+        }
+        rb.velocity=new Vector2(r*Movespeed,rb.velocity.y );
+        
     }
 }
