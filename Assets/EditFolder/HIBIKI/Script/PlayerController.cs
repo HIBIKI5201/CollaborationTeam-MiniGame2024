e4520e60bb4ac2fc81e603db909c8964b5f413f5
HIBIKI_5201 <takuto5201@gmail.com>
2024-05-30 13:00:26 +0900

    [update]壁登り中のステータスを追加

--- Assets/EditFolder/HIBIKI/Script/PlayerController.cs
+++ Assets/EditFolder/HIBIKI/Script/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("プレイヤーのコンポーネント")]
     [SerializeField] Rigidbody2D PlayerRB;
+    [SerializeField] SpriteRenderer PlayerRenderer;
 
     [Header("プレイヤーのステータス")]
     [SerializeField] float _moveSpeed = 5;
@@ -29,6 +30,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _bulletDamage;
     [SerializeField] float _bulletSpeed;
 
+    [Header("プレイヤーのモード")]
+    [SerializeField] PlayerMode playerMode;
+    public enum PlayerMode
+    {
+        Normal,
+        WallRun
+    }
+    bool WallRunning;
+
+    [Space]
+    [SerializeField] Sprite NormalSprite;
+    [SerializeField] Sprite WallRunSprite;
+ 
     [Header("他コンポーネント")]
     [SerializeField] Tilemap tilemap;
 
@@ -41,7 +55,7 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            if(jumpCount > 0)
+            if (jumpCount > 0)
             {
             jumpCount = 0;
             }
@@ -78,11 +92,33 @@ public class PlayerController : MonoBehaviour
                 Debug.Log($"タイルが見つからない\n座標：{cellPosition}\n法線方向：{CollisionNormal}\n\n{hitPosition}と{new Vector3Int((int)Mathf.Sign(CollisionNormal.x) * -1, (int)Mathf.Sign(CollisionNormal.y) * -1, 0)}");
             }
 
-
+            //壁登り
             if ((Mathf.Abs(CollisionNormal.x) >= 0.01 ? (int)Mathf.Sign(CollisionNormal.x) * -1 : 0) == Input.GetAxisRaw("Horizontal") && Input.GetAxisRaw("Horizontal") != 0)
             {
                 Debug.Log("壁登り");
+                if (!WallRunning)
+                {
+                    WallRunning = true;
+                    playerMode = PlayerMode.WallRun;
+                    PlayerRenderer.sprite = WallRunSprite;
+                }
+
                 PlayerRB.velocity = new Vector2(0, _wallclimbSpeed);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (WallRunning)
+            {
+                Debug.Log("壁登り解除");
+
+                WallRunning = false;
+                playerMode = PlayerMode.Normal;
+                PlayerRenderer.sprite = NormalSprite;
             }
         }
     }
@@ -103,7 +139,7 @@ public class PlayerController : MonoBehaviour
     {
         //左右移動
         float horizontal = Input.GetAxisRaw("Horizontal");
-        if(horizontal != 0)
+        if (horizontal != 0)
         {
             PlayerRB.velocity = new Vector2(_moveSpeed * horizontal, PlayerRB.velocity.y);
             transform.localScale = new Vector2(ScaleX * horizontal, transform.localScale.y);
@@ -115,7 +151,7 @@ public class PlayerController : MonoBehaviour
         }
 
         //ジャンプ
-        if(Input.GetKeyDown(KeyCode.Space) && jumpCount < _jumpLimit)
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < _jumpLimit)
         {
             PlayerRB.velocity = new Vector2(PlayerRB.velocity.x, 0);
             PlayerRB.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
@@ -124,19 +160,19 @@ public class PlayerController : MonoBehaviour
         }
 
         //伏せ
-        if(Input.GetKeyDown(KeyCode.S) && jumpCount == 0)
+        if (Input.GetKeyDown(KeyCode.S) && jumpCount == 0)
         {
             Debug.Log("伏せ");
         }
 
         //近接攻撃
-        if(Input.GetKeyDown(KeyCode.RightShift))
+        if (Input.GetKeyDown(KeyCode.RightShift))
         {
             StartCoroutine(Attack());
         }
 
         //遠距離攻撃
-        if(Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("遠距離攻撃");
 
