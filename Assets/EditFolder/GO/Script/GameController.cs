82834b20e1ca0d1b86d4766ccc278858358f2d0a
ごう\go008 <go00882244go@gmail.com>
2024-07-21 16:17:25 +0900

    [add] tirle scene 編集 [add] title scene script 編集

--- Assets/EditFolder/GO/Script/GameController.cs
+++ Assets/EditFolder/GO/Script/GameController.cs
@@ -8,29 +8,54 @@ public class GameController : MonoBehaviour
     [SerializeField] GameObject stageCanvas;
     [SerializeField] GameObject titleCanvas;
 
-    // Start is called before the first frame update
+    AudioSource _audio;
+    
     void Start()
     {
         stageCanvas.SetActive(false);
+        _audio = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Return) && stageCanvas.active)
+        {
+            _audio.Play();
+            GetStage1();
+        }
+
         if (Input.GetKeyDown(KeyCode.Return) && titleCanvas.active)
         {
-            StageSelectUiActive();
+            _audio.Play();
+            GetStageSelectUiActive();
             Debug.Log("aa");
         }
     }
 
-    public void StageSelectUiActive()
+    private void StageSelectUiActive()
     {
         stageCanvas.SetActive(true);
         titleCanvas.SetActive(false);
     }
-    public void Stage1 ()
+
+    public void GetStageSelectUiActive()
+    {
+        Invoke(nameof(StageSelectUiActive), 0.5f);
+    }
+
+    private void Stage1()
     {
         SceneManager.LoadScene("GOscene");
+    }
+
+    public void GetStage1()
+    {
+        Invoke(nameof(Stage1), 0.5f);
+    }
+
+    public void Audio()
+    {
+        _audio.Play();
     }
 }
